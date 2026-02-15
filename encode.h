7b055edc9068758579ae39b65f316abd84ef2454
susmithah117 <susmithah117@gmail.com>
2026-02-15 21:35:49 +0530

    Update encode.h

--- encode.h
+++ encode.h
@@ -3,12 +3,6 @@
 
 #include "types.h" // Contains user defined types
 
-/* 
- * Structure to store information required for
- * encoding secret file to source Image
- * Info about output and intermediate data is
- * also stored
- */
 
 #define MAX_SECRET_BUF_SIZE 1
 #define MAX_IMAGE_BUF_SIZE (MAX_SECRET_BUF_SIZE * 8)
@@ -75,12 +69,6 @@ Status encode_secret_file_size(int file_size, EncodeInfo *encInfo);
 
 /* Encode secret file data*/
 Status encode_secret_file_data(EncodeInfo *encInfo);
-
-/* Encode function, which does the real encoding */
-//Status encode_data_to_image(char *data, int size, FILE *fptr_src_image, FILE *fptr_stego_image);
-
-/* Encode a byte into LSB of image data array */
-//Status encode_byte_to_lsb(char data, char *image_buffer);
 
 /* Copy remaining image bytes from src to stego image after encoding */ 
 Status copy_remaining_img_data(FILE *fptr_src, FILE *fptr_dest);
